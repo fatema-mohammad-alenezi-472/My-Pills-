96f6e2e0b9f327e4c07b498e893846efcd302971
fatema-mohammad-alenezi-472 <66527004+fatema-mohammad-alenezi-472@users.noreply.github.com>
2020-07-19 19:01:52 +0300

    Initial commit

---
+++ My Pills!Tests/My_Pills_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  My_Pills_Tests.swift
+//  My Pills!Tests
+//
+//  Created by Mohammad Alanazi on 7/19/20.
+//  Copyright © 2020 fatimah alenezi. All rights reserved.
+//
+
+import XCTest
+@testable import My_Pills_
+
+class My_Pills_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
